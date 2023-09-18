8786cb1c45863e10f38e63135d007fa013eb0791
Haja3180 <129274543+Haja3180@users.noreply.github.com>
2023-09-18 12:44:09 +0200

    ajout de la clé timestamps au model post

--- model/post_model.js
+++ model/post_model.js
@@ -1,20 +1,31 @@
 //post model
 import mongoose from 'mongoose';
 
-const postschema = new mongoose.Schema({
-    post_title: {
-        type: String,
-        required: true
-        // trim: true
-    }, 
-    post_author: {
-        type: String,
-        required: true
+const postschema = new mongoose.Schema(
+    {
+        post_title: {
+            type: String,
+            required: true
+        }, 
+
+        post_author: {
+            type: String,
+            required: true
+        },
+
+        post_content: {
+            type: String,
+            required : false
+        },
+
+        post_likers: {
+            type: [String]
+        }
     },
-    post_likers: {
-        type: [String]
+    {
+        timestamps: true
     }
-});
+    );
 
 //definition de notre model à enregistrer
 export const Post = mongoose.model('Post', postschema);
