d29a29d0514510c8ca009a434ce102ea93715644
Haja3180 <129274543+Haja3180@users.noreply.github.com>
2023-09-18 15:44:18 +0200

    modif mineur

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-//app
+//fichier principal (clé main dans package.json)
 
 import express from "express" //import du module express selon docs https://expressjs.com/en/starter/hello-world.html
 import { connect_db } from './db/db_connection.js' //import de la variable connect_db depuis le répertoire indiqué
 import { post_router } from "./routes/post_route.js" //import de la variable post_router
 import { user_router } from "./routes/user_route.js" //import de la variable user_router
+
 connect_db(); // appel de la fonction connect_db précédemment importée
 
 //initialisation serveur express (cf. docs expressJS https://expressjs.com/en/starter/hello-world.html)
 const app = express();
 const port = 4000;
 
-//spécifie à l'app qu'on utilise du json
+// //spécifie à l'app qu'on utilise du json
 app.use(express.json())
 
 //spécifie à l'app d'utiliser la route posts (incluant la f° d'ajout de post)
